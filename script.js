19c4f23aecea898128244fa0d3d9344bbfa6549b
Alejandro <betancorta666@gmail.com>
2025-10-14 08:39:27 +0100

    botones funcionan pero no cambian

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
-var maximo = 9;
-var minimo = 1;
-var select = 0;
 
-imagenNUla = document.getElementById("nulo");
-select = imagenNUla.parentNode.id;             // ENCUENTRA EL HUECO Y ELIGE EL ID DE LA TABLA
-
-console.log(select);
-if(select -3 < minimo){
-    opcion1 = select+3;
+function getbuttons(){
+    var maximo = 9;
+    var minimo = 1;
+    imagenNUla = document.getElementById("nulo").parentNode.id;
+    console.log(imagenNUla);
+if(imagenNUla -3 < minimo){
+    opcion1 = imagenNUla+3;
 }else{
-    opcion1 = select-3;
+    opcion1 = imagenNUla-3;
 }
-opcion2 = select -1;
-console.log(opcion1 + " " + opcion2);           //CALCULA LOS CLICKABLES BASADOS EN DONDE ESTA EL HUECO
+opcion2 = imagenNUla -1;
+console.log(opcion1 + " " + opcion2);  
+    boton1 = document.getElementById(opcion1);
+    boton2 = document.getElementById(opcion2);
+}
 
-boton1 = document.getElementById(opcion1);
-boton2 = document.getElementById(opcion2);
-boton1.addEventListener('click', function() {  
-    contentTable = document.getElementById(boton1.id).innerHTML;
+getbuttons();
+boton1.onclick = function(){  
+    document.getElementById(document.getElementById("nulo").parentNode.id).innerHTML = "<img src='imgs/0"+boton1.id+".jpg'></img>"
     document.getElementById(boton1.id).innerHTML = "<img id = 'nulo' src='imgs/09.jpg'></img>";
-    document.getElementById(imagenNUla.parentNode.id).innerHTML = "<img src='imgs/0"+boton1.id+".jpg'></img>"
-});
-boton2.addEventListener('click', function() {  
-    contentTable = document.getElementById(boton2.id).innerHTML;
+    getbuttons();
+};
+boton2.onclick = function(){
+    document.getElementById(document.getElementById("nulo").parentNode.id).innerHTML = "<img src='imgs/0"+boton2.id+".jpg'></img>"
     document.getElementById(boton2.id).innerHTML = "<img id = 'nulo' src='imgs/09.jpg'></img>";
-    document.getElementById(imagenNUla.parentNode.id).innerHTML = "<img src='imgs/0"+boton2.id+".jpg'></img>"
-});                             //ASIGNA BOTON A LAS OPCIONES CLICKABLES :)
-
-//imageswap
+    getbuttons();
+};   
 
