382600e79d432909b67fbb96efd5dec5caf6df40
Alejandro <77583757+betanS@users.noreply.github.com>
2025-10-26 20:41:13 +0000

    Arreglo todo, cambio de logica en 90% del codigo y poco más.

--- randomize.js
+++ randomize.js
@@ -1,41 +1,59 @@
-//assingRandomImgs()
-assingTestValues();
+assingRandomImgs()  ;  //Usar para randomizar las imagenes /(puede tener output no completable)/ /(asigna variables que no corresponden a las id de imagen)/
+//localStorage.clear();  //Usar para limpiar el local storage en cada carga de pagina (testeo)
+//assingTestValues(); //Usar para testear con valores fijos 
 
 function assingTestValues(){
+    const arrayposiciones = [];
 for (i = 1; i< 10; i++) {
     if (document.getElementById(i).innerHTML == ""){
         if (i == 8){
+            arrayposiciones.push(9);
         document.getElementById(i).innerHTML = "<img class = 'nulo' src='imgs/09.jpg'></img>";
         }else if (i == 9){
+            arrayposiciones.push(8);
             document.getElementById(i).innerHTML = "<img class= '8' src='imgs/08.jpg'></img>";
         }else{
+            arrayposiciones.push(""+i+"");
             document.getElementById(i).innerHTML = "<img class= '"+i+"' src='imgs/0"+i+".jpg'></img>";
         }
     };
 }
+saveData(arrayposiciones);
+
 }
 
 
 
-function assingRandomImgs() {
-var contador = 1;
-while (contador != 10) {
-    numerorandom = getRandomInt(1, 10);
-    if (document.getElementById(numerorandom).innerHTML == ""){
-        if (contador == 9){
-        document.getElementById(numerorandom).innerHTML = "<img class= 'nulo' src='imgs/09.jpg'></img>";
+function assingRandomImgs() {  //Usar para randomizar las imagenes, [!!puede tener output no completable!!]
+const arrayposiciones = [];
+    var posTabla = 1;
+    document.getElementById(9).innerHTML = "<img class= 'nulo' src='imgs/09.jpg'></img>"; //Asegura que siempre haya un espacio vacio 
+while (posTabla != 9) {
+    numeroImagen = getRandomInt(1, 10); //Genera un numero aleatorio entre 1 y 9
+    if (document.getElementById(posTabla).innerHTML == "" && !arrayposiciones.includes(numeroImagen)){ { //Evita repetir numeros y sobreescribir posiciones 
+        if (numeroImagen == 9){
+            continue; //Evita asignar el espacio vacio
         }else{
-            document.getElementById(numerorandom).innerHTML = "<img class= '"+contador+"' src='imgs/0"+contador+".jpg'></img>";
+            arrayposiciones[posTabla-1] = numeroImagen; //Guarda la posicion en el array
+            document.getElementById(posTabla).innerHTML = "<img class= '"+numeroImagen+"' src='imgs/0"+numeroImagen+".jpg'></img>"; //Asigna la imagen al HTML
+            posTabla++; //Solo avanza si se ha asignado una posicion correctamente
         }
-        contador++;
-    };
+    }
+}  
 }
-    
-}
+arrayposiciones[8] = 9; //Añade el espacio vacio al final
+        console.log(arrayposiciones);
+    saveData(arrayposiciones);
+ }
 
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min);
+}
+
+function saveData(elarray){                 //Guarda el array en el localStorage
+  const datos = [elarray] 
+  localStorage.setItem("Data", JSON.stringify(datos))
 }
--- script.js
+++ script.js
@@ -1,27 +1,79 @@
+let tiempo = 0;
+let intervaloTiempo = null;
 
-    const botones = document.querySelectorAll('.botones');
-    botones.forEach(function(boton) {
-        boton.addEventListener('click', function(event) {
-            const id = event.currentTarget.id;
-            console.log ("Posición Clickada  " + id)
-            posicionNula = document.getElementById("nulo").parentNode.id;
-            console.log("Posición Nula: " + posicionNula);
-            const posibilidades = getbuttons(posicionNula);
-            //console.log(posibilidades);
-            if (id == posibilidades[0] || id == posibilidades[1] || id == posibilidades[2] || id == posibilidades[3]) {
+
+function iniciarContador(inicio = 0) {
+  // asigna el valor inicial (por si se pasa desde cargarPartida)
+  tiempo = inicio;
+
+  // si ya hay un intervalo corriendo, lo detenemos
+  if (intervaloTiempo) clearInterval(intervaloTiempo);
+
+  // iniciamos un nuevo intervalo
+  intervaloTiempo = setInterval(() => {
+    tiempo++;
+    document.getElementById("tiempo").innerHTML =
+      "Tiempo transcurrido: " + tiempo + " segundos";
+  }, 1000);
+}
+iniciarContador(); // inicia el contador al cargar la página
+
+
+
+
+
+const modal = document.getElementById('modal');
+
+
+    arrayposiciones2 = JSON.parse(localStorage.getItem("Data"))[0];
+    console.log("Array de posiciones cargado desde localStorage: " + arrayposiciones2);
+    
+    cargarLeaderboard();
+    
+    const botones = document.querySelectorAll('.botones');   //selecciona todos los clickables
+    botones.forEach(function(boton) {                        //Logica por boton   
+        boton.addEventListener('click', function(event) {   //añade un click a cada boton
+            var id = event.currentTarget.id;
+           
+            posicionNula = document.getElementsByClassName("nulo")[0].parentNode.id; //Obtiene la posición del cuadro nulo
+            
+            const posibilidades = getbuttons(posicionNula);                         //Obtiene las posiciones que se pueden clicar
+            if (id == posibilidades[0] || id == posibilidades[1] || id == posibilidades[2] || id == posibilidades[3]) { 
                     console.log("SI SE PUEDE REEMPLAZAR!");
+                
+                    //Obtener los valores en el array de posiciones
+                    valornulo = parseInt(posicionNula) - 1;
+                    valorclicado = parseInt(id) - 1;
                     document.getElementById(posicionNula).innerHTML = document.getElementById(id).innerHTML;     //NO ME PUEDO CREER QUE EN VEZ DE REEMPLAZAR TODO EL CONTENIDO SOLO REMPLAZABA LA ID DE LA IMAGEN POR ID DE POSICION XD
-                    document.getElementById(id).innerHTML = "<img id = 'nulo' src='imgs/09.jpg'></img>";
+                    document.getElementById(id).innerHTML = "<img class = 'nulo' src='imgs/09.jpg'></img>";
+
+                    //Actualizar el array de posiciones
+                    arrayposiciones2[valornulo] = arrayposiciones2[valorclicado]; 
+                    arrayposiciones2[valorclicado] = 9;
+
+
+/*Pensé que mejor que acceder al html y sacar ids o class, 
+seria que estuvieran preasignados y fueran cambiando "a ciegas" 
+de lo que pasa realmente (en los on click que cambiara X  Y 
+porque al generar la tabla ya se asignaron sus valores) */
+
+
+
             }else{
                 console.log("No se puede reemplazar la posición " + id + "  :(");
             }
+
+
             if (comprobarPosiciones()){
-            console.log("FELICIDADES!!! COMPLETASTE EL PUZZLE, YIPPIE!!!")
+            console.log("FELICIDADES!!! COMPLETASTE EL PUZZLE, YIPPIE!!!");
+            modal.classList.remove('hidden');                               //Muestra el modal
+            clearInterval(intervaloTiempo);                                 //Detiene el contador
+            document.getElementById("finalTime").innerText = tiempo + " segundos!";       //Muestra el tiempo final en el modal
         }
         });
     });
 
-function getbuttons(imagenNUla){
+function getbuttons(imagenNUla){                                                //Obtiene las posiciones que se pueden clicar
 switch(parseInt(imagenNUla)) {
   case 1:
     opcion1 = 2;
@@ -89,54 +141,112 @@ case 7:
         console.log("Posibles Posiciones Clickables: " + opcion1 + " " + opcion2 + " " + opcion3 + " " + opcion4);  
     return opciones;
 }
-//arrayCasillas=[1,2,3,4,5,6,7,8,9]
-//arrayImagenes["01.jpg", ...b]
 
-function comprobarPosiciones() {
-    var posicionesincorrectas = "";
-for (i = 1; i < 10; i++) {
-    var imagen = document.getElementById(i).innerHTML
-    console.log(imagen.getAttribute("class").value);
-    if(imagen.getAttribute("class").value != i){
-        posicionesincorrectas += ""+i+"  is not " + imagen.getAttribute("class").value + ", ";
+//################### LOGICA BOTONES MODAL Y GUARDAR/CARGAR PARTIDA #############
+botonGuardarLeaderboard = document.getElementById("guardarNombreJugador");      
+botonGuardarLeaderboard.addEventListener('click', function() {
+    var nombreJugador = document.getElementById("nombreJugador").value;
+    if (nombreJugador == ""){
+        alert("Por favor, ingresa un nombre de jugador.");
+        return;
+    }else if (localStorage.getItem(nombreJugador)) {
+        alert("El nombre de jugador ya existe.");
+        return;
     }
-}
-if(posicionesincorrectas != ""){
-    console.log(posicionesincorrectas);
-    return false;
-}else{  
+    localStorage.setItem(nombreJugador, tiempo);
+    
+    
+
+    var leaderboardArray = JSON.parse(localStorage.getItem("Leaderboard"));
+    if (leaderboardArray == null){
+        leaderboardArray = [];
+    }
+    leaderboardArray.push({nombre: nombreJugador, tiempo: tiempo});
+    localStorage.setItem("Leaderboard", JSON.stringify(leaderboardArray));
+    console.log("Jugador guardado en el leaderboard: " + nombreJugador);
+    tiempo = 0; // Reiniciar el tiempo para una nueva partida
+    cargarLeaderboard();
+    modal.classList.add('hidden');
+});
+
+botonGuardar = document.getElementById("guardar");
+botonGuardar.addEventListener('click', function() {
+    guardarPartida();
+});
+botonCargar = document.getElementById("cargar");
+botonCargar.addEventListener('click', function() {
+    cargarPartida();
+});
+
+
+
+
+//################### FUNCIONES AUXILIARES #############
+
+function comprobarPosiciones() {                               //Comprueba si las posiciones son correctas 
+   console.log(arrayposiciones2);
+    for (let i = 1; i < 10; i++) {
+        if(arrayposiciones2[i - 1] != i){
+            return false;
+        }
+    }
     console.log("posiciones correctas");
     return true;
 }
+
+
+function findPositionInArray(valor, array) {                  //Encuentra la posicion de un valor en un array
+    var found = 0;
+    console.log(array.length);
+    for(var i = 0; i < array.length; i++) {
+        if(valor == array[i]) found = i;
+    }
+    console.log("Valor " + valor + " encontrado en la posicion " + found);
+    return found;
 }
 
 
 
-/*
-boton1.addEventListener("click", function() {
-    document.getElementById(document.getElementById("nulo").parentNode.id).innerHTML = "<img src='imgs/0"+boton1.id+".jpg'></img>"
-    document.getElementById(boton1.id).innerHTML = "<img id = 'nulo' src='imgs/09.jpg'></img>";
-getbuttons();
-});
-boton2.addEventListener("click", function() {
-    document.getElementById(document.getElementById("nulo").parentNode.id).innerHTML = "<img src='imgs/0"+boton2.id+".jpg'></img>"
-    document.getElementById(boton2.id).innerHTML = "<img id = 'nulo' src='imgs/09.jpg'></img>";
-getbuttons();
-});
-if(opcion3 != 0){
-boton3.addEventListener("click", function() {
-    document.getElementById(document.getElementById("nulo").parentNode.id).innerHTML = "<img src='imgs/0"+boton3.id+".jpg'></img>"
-    document.getElementById(boton3.id).innerHTML = "<img id = 'nulo' src='imgs/09.jpg'></img>";
-getbuttons();
-});
-} 
-if(opcion4 != 0){
-    boton4.addEventListener("click", function() {
-    document.getElementById(document.getElementById("nulo").parentNode.id).innerHTML = "<img src='imgs/0"+boton4.id+".jpg'></img>"
-    document.getElementById(boton4.id).innerHTML = "<img id = 'nulo' src='imgs/09.jpg'></img>";
-getbuttons();
-});
-}*/
 
-  
 
+function guardarPartida(){                                                  //Guarda la partida en el localStorage
+    var arrayPartida = [arrayposiciones2, tiempo];
+    localStorage.setItem("PartidaGuardada", JSON.stringify(arrayPartida));
+    
+    console.log("Partida guardada correctamente");
+}
+
+function cargarPartida(){                                             //Carga la partida desde el localStorage
+    datos = JSON.parse(localStorage.getItem("PartidaGuardada"));
+    arrayposiciones2 = datos[0];
+    tiempo = datos[1];
+    if (arrayposiciones2 == null){
+        alert("No hay partida guardada");
+        return;
+    }
+    for (let i = 1; i < 10; i++) {                                  //Actualiza el HTML con las posiciones guardadas
+        if (arrayposiciones2[i - 1] == 9){
+            document.getElementById(i).innerHTML = "<img class = 'nulo' src='imgs/09.jpg'></img>";
+        }else{
+            document.getElementById(i).innerHTML = "<img class= '"+arrayposiciones2[i - 1]+"' src='imgs/0"+arrayposiciones2[i - 1]+".jpg'></img>";
+        }
+    }
+    console.log("Partida cargada correctamente: " + arrayposiciones2);
+}
+
+
+function cargarLeaderboard(){                                               //Carga el leaderboard desde el localStorage
+    var leaderboardArray = JSON.parse(localStorage.getItem("Leaderboard"));
+    if (leaderboardArray == null){                                            //Si no existe, crea un array vacio
+        leaderboardArray = [];
+    }
+
+    leaderboardArray.sort((a, b) => a.tiempo - b.tiempo);               //Ordena el array por tiempo ascendente
+
+    document.getElementById("leaderboard").innerHTML = "<b>Leaderboard:</b>";              //Limpia el HTML del leaderboard para evitar duplicados (ej al cargar tras guardar)
+
+    for (let i = 0; i < leaderboardArray.length; i++) {             //Muestra el leaderboard en el HTML
+        document.getElementById("leaderboard").innerHTML += "<li>" + leaderboardArray[i].nombre + ": " + leaderboardArray[i].tiempo + " segundos</li>";
+    }
+    console.log("Leaderboard cargado correctamente");
+}
